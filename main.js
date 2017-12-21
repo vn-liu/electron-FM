8c585c826a44bc4ae25fa8f1c1d5a8f318a7acdb
刘立铭 <694467256@qq.com>
2017-12-21 10:33:55 +0800

    add transparent titlebar

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -17,7 +16,8 @@ function createWindow () {
         protocol: 'file:',
         slashes: true
     }) //开发环境开启热更新
-    mainWindow = new BrowserWindow({width: 1000, height: 800, title: app.getName()})
+    mainWindow = new BrowserWindow({width: 1000, height: 800, title: app.getName(),maxHeight: 800,fullscreenable: false,
+        maxHeight: 900, titleBarStyle: 'hidden', backgroundColor: '#fff'})
     // and load the index.html of the app.
     mainWindow.loadURL(openUrl)
     app.setAboutPanelOptions({//macOS
